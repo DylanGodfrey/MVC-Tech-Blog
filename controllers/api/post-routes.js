f6e5a4085aa00840e4ffc67aac08d521b6b35e52
DylanGodfrey <39961099+DylanGodfrey@users.noreply.github.com>
2022-11-22 23:08:36 -0500

    Updated README

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,120 +1,119 @@
-const router = require('express').Router();
-const { User, Post, Comment } = require('../../models');
-const withAuth = require('../../utils/auth');
+const router = require("express").Router();
+const { User, Post, Comment } = require("../../models");
+const withAuth = require("../../utils/auth");
 
 //GET All Posts
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const allPost = await Post.findAll({
-      attributes: ['id', 'title', 'content'],
+      attributes: ["id", "title", "content"],
       include: [
         {
           model: User,
-          attributes: ['username']
+          attributes: ["username"],
         },
         {
           model: Comment,
-          attributes: ['id', 'comment_text', 'post_id', 'user_id'],
+          attributes: ["id", "comment_text", "post_id", "user_id"],
           include: {
             model: User,
-            attributes: ['username']
-          }
-        }
-      ]
-    })
+            attributes: ["username"],
+          },
+        },
+      ],
+    });
     if (allPost) {
-      res.json(allPost.reverse())
+      res.json(allPost.reverse());
     }
   } catch (err) {
-    res.status(500).json(err)
+    res.status(500).json(err);
   }
 });
 
 //GET Single Post
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   try {
     const onePost = await Post.findOne({
       where: {
-        id: req.params.id
+        id: req.params.id,
       },
-      attributes: ['id', 'content', 'title'],
+      attributes: ["id", "content", "title"],
       include: [
         {
           model: User,
-          attributes: ['username']
+          attributes: ["username"],
         },
         {
           model: Comment,
-          attributes: ['id', 'comment_text', 'post_id', 'user_id'],
+          attributes: ["id", "comment_text", "post_id", "user_id"],
           include: {
             model: User,
-            attributes: ['username']
-          }
-        }
-      ]
-    })
+            attributes: ["username"],
+          },
+        },
+      ],
+    });
     if (!onePost) {
-      res.status(400).json({ message: 'Sorry no post with this id' })
+      res.status(400).json({ message: "Sorry no post with this id" });
     }
-    res.json(onePost)
+    res.json(onePost);
   } catch (err) {
-    res.status(500).json(err)
+    res.status(500).json(err);
   }
 });
 
 // CREATE Post withAuth
-router.post('/', withAuth, async (req, res) => {
+router.post("/", withAuth, async (req, res) => {
   try {
     const newPost = await Post.create({
       title: req.body.title,
       content: req.body.content,
-      user_id: req.session.user_id
-    })
-    res.json(newPost)
+      user_id: req.session.user_id,
+    });
+    res.json(newPost);
   } catch (err) {
-    res.status(500).json(err)
+    res.status(500).json(err);
   }
 });
 
-
 // UPDATE Post
-router.put('/:id', withAuth, async (req, res) => {
+router.put("/:id", withAuth, async (req, res) => {
   try {
     const editPost = await Post.update(
       {
         title: req.body.title,
-        content: req.body.content
+        content: req.body.content,
       },
       {
         where: {
-          id: req.params.id
-        }
+          id: req.params.id,
+        },
       }
-    )
+    );
     if (!editPost) {
-      res.status(400).json({ message: 'Cannot find post with this id' })
+      res.status(400).json({ message: "Cannot find post with this id" });
     }
-    res.json(editPost)
+    res.json(editPost);
   } catch (err) {
-    res.status(500).json(err)
+    res.status(500).json(err);
   }
 });
 
 //DELETE the users post
-router.delete('/:id', withAuth, async (req, res) => {
+router.delete("/:id", withAuth, async (req, res) => {
   try {
     const deletePost = Post.destroy({
       where: {
-        id: req.params.id
-      }
-    })
+        id: req.params.id,
+      },
+    });
     if (!deletePost) {
-      res.status(400).json({ message: 'Cannot find id for this post' })
+      res.status(400).json({ message: "Cannot find id for this post" });
     }
-    res.json(deletePost)
+    res.json(deletePost);
   } catch (err) {
-    res.status(500).json(err)
+    res.status(500).json(err);
   }
 });
 
-module.exports = router
+module.exports = router;
